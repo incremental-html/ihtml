7fc440b0e4410b2b3a1b784c2b94dfd686224ed4
Incremental HTML <->
2023-12-25 13:48:53 +0100

    Docs examples

--- docs/examples/execute-examples.sh
+++ docs/examples/execute-examples.sh
@@ -6,23 +6,17 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 export PATH="$PATH:$SCRIPT_DIR/../.."
 set -o xtrace
 
-cd doc/examples/projects/ || exit
+cd docs/examples/ || exit
 #
 # project example
 #
-ihtml -p example-website/example-website-project/ \
-      -o example-website/example-website-generated/
+ihtml -p example-project-website/project/ \
+      -o example-project-website/generated/
 #
 # single file example
 #
-ihtml ../static-html-bundles/html5up-stellar/generic.html \
-      ccs/white-space.ccs \
-      -o ccs/examples/generated.html
-# to see results:
-# diff -w static-html-bundles/html5up-stellar/generic.html ccs/examples/generated.html
-
-ihtml ../static-html-bundles/html5up-stellar/generic.html \
+ihtml static-html-bundles/html5up-stellar/generic.html \
       example-file-whitespace/white-space.ccs \
       -o example-file-whitespace/generated.html
 # to see results:
-# diff -w static-html-bundles/html5up-stellar/generic.html ccs/examples/generated.html
+# diff -w static-html-bundles/html5up-stellar/generic.html example-file-whitespace/generated.html
