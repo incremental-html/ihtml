ecc2e5458a2e060a625acfc479cf9f7db28b4de2
Incremental HTML <->
2023-12-31 15:10:58 +0100

    Refactoring

--- src/CcsProperty/ClassProperty.php
+++ src/CcsProperty/ClassProperty.php
@@ -25,40 +25,61 @@ class ClassProperty extends Property
      */
     public static function apply(Crawler $list, array $params): void
     {
+        $classesVisibility = self::declarationParamsToMap($params);
         foreach ($list as $element) {
             /** @var DOMElement $element */
-            if (count($params) % 2 > 0) {
-                throw new Exception('Wrong `class` property values count');
-            }
-            $params = array_chunk($params, 2);
-            foreach ($params as [$class, $visibility]) {
-                if (!is_string($class)) {
-                    throw new Exception("Wrong `class` name ($class)");
-                }
-                if (!in_array($visibility, [self::VISIBLE, self::HIDDEN])) {
-                    throw new Exception("Wrong `class` visibility ($class)");
-                }
-                $classList = $element->getAttribute('class');
-                $classList = (array)preg_split('/\s+/', $classList);
-                $classList = array_filter($classList);
-                $classList = array_flip($classList);
+            $classAttribute = $element->getAttribute('class');
+            $classesMap = self::classAttributeToMap($classAttribute);
+            foreach ($classesVisibility as $class => $visibility) {
                 switch ($visibility) {
                     case self::VISIBLE:
-                        $classList[$class] = true;
+                        $classesMap[$class] = true;
                         break;
                     case self::HIDDEN:
-                        unset($classList[$class]);
+                        unset($classesMap[$class]);
                         break;
-                    default:
                 }
-                $classList = array_keys($classList);
-                $classList = implode(' ', $classList);
-                $element->setAttribute('class', $classList);
             }
+            $classAttribute = self::classMapToAttribute($classesMap);
+            $element->setAttribute('class', $classAttribute);
         }
     }
 
     public static function render(DOMDocument $domDocument): void
     {
+    }
+
+    private static function classAttributeToMap(string $classList): array
+    {
+        $classList = (array)preg_split('/\s+/', $classList);
+        $classList = array_filter($classList);
+        $classList = array_flip($classList);
+        return $classList;
+    }
+
+    private static function classMapToAttribute(array $classesMap): string
+    {
+        $classList = array_keys($classesMap);
+        $classList = implode(' ', $classList);
+        return $classList;
+    }
+
+    public static function declarationParamsToMap(array $params): array
+    {
+        if (count($params) % 2 > 0) {
+            throw new Exception('Wrong `class` property values count');
+        }
+        $params = array_chunk($params, 2);
+        $classesVisibility = [];
+        foreach ($params as [$class, $visibility]) {
+            if (!is_string($class)) {
+                throw new Exception("Wrong `class` name ($class)");
+            }
+            if (!in_array($visibility, [self::VISIBLE, self::HIDDEN])) {
+                throw new Exception("Wrong `class` visibility ($class)");
+            }
+            $classesVisibility[$class] = $visibility;
+        }
+        return $classesVisibility;
     }
 }
